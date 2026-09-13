5ebdd012b5c7b79e0cb0b26498bbdb801bd81738
Angie Wang <aw1407@georgetown.edu>
2026-09-13 11:01:18 -0400

    Revise lab group details and improve comments
    Updated lab group members and clarified comments for better readability.

--- Lab01_MATH2650.R
+++ Lab01_MATH2650.R
@@ -1,15 +1,18 @@
+###############################################################################
 #
 #
 #   MATH 2650: Computational Statistics
 #   Lab 01
 #   10 September, 2026
 # 
-#   First Lab Group 1:
-#   Matt O'Connor, Vonn Russell, Angie Wang
+#   First Lab Group 1
+#   Angie Wang
 #
 #
-#
+###############################################################################
 
+# Question 1: Use uniroot() function to calculate MLE for Bernoulli and Exponential
+#             distributions. Compare results to the traditional analytical approach.
 
 #### (1a.) uniroot Bernoulli ####
 mag <- 1*(attenu$mag >6) # Obtain all magnitudes greater than 6
@@ -53,26 +56,30 @@ exp_root <- uniroot(calc_deriv_exp, x = accel, lower = 0, upper = 10)
 numeric_lambda <- exp_root$root
 numeric_lambda
 
+###############################################################################
+
+# Question 2: Use the Probability Integral Transform method to generate random 
+#             numbers from Gumbel and Lomax distributions.
+
 #### (2a.) PIT Gumbel ####
 
 # Constants:
 N <- 20000 # Number of random draws
-BETA_1 <- 1 # Testing with beta = 1 and 3
+BETA_1 <- 1 # Testing with beta = 1 and beta = 3
 BETA_3 <- 3
 
 z <- runif(N) # Obtain N draws from uniform distribution
 
 calc_gumbel_inv <- function(z, beta){
   # Inverse gumbel cdf
-  
   -beta*log(-log(z))
 }
 
-
+# Test with parameters beta = 1 and beta = 3
 gumbel_dist_beta1 <- calc_gumbel_inv(z = z, beta = BETA_1)
 gumbel_dist_beta3 <- calc_gumbel_inv(z = z, beta = BETA_3)
 
-
+# Generate histograms
 hist(gumbel_dist_beta1, freq = FALSE, main = paste(N, ' samples', sep = ''))
 lines(density(gumbel_dist_beta1), lwd = 2, col = 'darkblue')
 
@@ -84,29 +91,36 @@ lines(density(gumbel_dist_beta3), lwd = 2, col = 'darkblue')
 # Constants:
 M <- 20000 # Number of random draws
 ALPHA <- 20 # Test with 20
-LAMBDA_5 <- 5 # Test with lambda = 5 and 10
+LAMBDA_5 <- 5 # Test with lambda = 5 and lambda = 10
 LAMBDA_10 <- 10 
 
 
 w <- runif(M) # Obtain M draws from uniform distribution
 
 calc_lomax_inv <- function(w, lambda, alpha){
+  # Inverse lomax cdf
   lambda*((1-w)^(-1/alpha) - 1) 
 }
 
-lomax_dist_lambda5 <- calc_lomax_inv(w = w, lambda = LAMBDA_5, alpha = ALPHA)
+# Test with parameters lambda = 5 and lambda = 10
+lomax_dist_lambda5 <- calc_lomax_inv(w = w, lambda = LAMBDA_5, alpha = ALPHA) 
 lomax_dist_lambda10 <- calc_lomax_inv(w = w, lambda = LAMBDA_10, alpha = ALPHA)
 
-
+# Generate histograms
 hist(lomax_dist_lambda5, freq = FALSE, main = paste(M, ' samples', sep = ''))
 lines(density(lomax_dist_lambda5), lwd = 2, col = 'darkblue')
 
 hist(lomax_dist_lambda10, freq = FALSE, main = paste(M, ' samples', sep = ''))
 lines(density(lomax_dist_lambda10), lwd = 2, col = 'darkblue')
 
+###############################################################################
+
+# Question 3: Generate a random number using the Middle-Square / Von Neumann
+#             method using a predetermined starting seed.
+
 #### (3.) Middle-Square / Von Neumann ####
 
-seed <- 812016
+seed <- 812016 # Starting seed
 
 seed_str <- as.character(seed)
 seed_len <- nchar(seed_str)
